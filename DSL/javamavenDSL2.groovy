9ce060707f5356425923a99be359eec6d3f04917
Mynor Xico <mynorottoniel.xico2@gmail.com>
2025-11-01 20:33:58 -0600

    Change Git repo and remove Slack notifier
    Updated Git repository URL and removed Slack notifier configuration.

--- DSL/javamavenDSL2.groovy
+++ DSL/javamavenDSL2.groovy
@@ -1,7 +1,7 @@
 job('Java Maven App DSL 2') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
+        git('https://github.com/MynorXico/simple-java-maven-app.git', 'master') { node ->
             node / gitConfigName('macloujulian')
             node / gitConfigEmail('macloujulian@gmail.com')
         }
@@ -23,22 +23,5 @@ job('Java Maven App DSL 2') {
     publishers {
         archiveArtifacts('target/*.jar')
         archiveJunit('target/surefire-reports/*.xml')
-	slackNotifier {
-            notifyAborted(true)
-            notifyEveryFailure(true)
-            notifyNotBuilt(false)
-            notifyUnstable(false)
-            notifyBackToNormal(true)
-            notifySuccess(true)
-            notifyRepeatedFailure(false)
-            startNotification(false)
-            includeTestSummary(false)
-            includeCustomMessage(false)
-            customMessage(null)
-            sendAs(null)
-            commitInfoChoice('NONE')
-            teamDomain(null)
-            authToken(null)
-       }
     }
 }
